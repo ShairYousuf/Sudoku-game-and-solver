37ba2e8bd02db3695b3fb33c9cc8e9301a30a32d
ShairYousuf <syjahin@mun.ca>
2021-07-02 00:49:13 +0600

    Gui made in pygame
    The gui made in pygame with the click option added

--- sudoku try 2/settings.py
+++ sudoku try 2/settings.py
@@ -3,6 +3,13 @@ HEIGHT=600
 
 #colors
 WHITE=(255,255,255)
+BLACK=(0,0,0)
+LIGHTBLUE=(96,216,232)
 
 #boards
-testBoard=[[0 for x in range(9)] for x in range(9)]
+testBoard1=[[0 for x in range(9)] for x in range(9)]
+
+# position and sizes
+gridPos = (75,100)
+cellSize = 50
+gridSize= cellSize*9
