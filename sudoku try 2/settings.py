2f228888d60197e15097a67f629408abc652ff9b
ShairYousuf <syjahin@mun.ca>
2021-07-02 00:59:23 +0600

    Initialisation

---
+++ sudoku try 2/settings.py
@@ -0,0 +1,15 @@
+WIDTH=600
+HEIGHT=600
+
+#colors
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+LIGHTBLUE=(96,216,232)
+
+#boards
+testBoard1=[[0 for x in range(9)] for x in range(9)]
+
+# position and sizes
+gridPos = (75,100)
+cellSize = 50
+gridSize= cellSize*9
