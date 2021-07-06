9f9cfac1fb37af22818e3a623c68b1611aa69a8b
ShairYousuf <syjahin@mun.ca>
2021-07-07 03:14:52 +0600

    Checking row function implemented and colour indication implemented

--- sudoku try 2/app_class.py
+++ sudoku try 2/app_class.py
@@ -7,15 +7,18 @@ class App:
         pygame.init()
         self.window=pygame.display.set_mode((WIDTH,HEIGHT))
         self.running=True
-        self.grid = testBoard2
+        self.grid = finishedBoard
         # print(self.grid)
         self.selected=None
         self.mousePos=None
         self.state="playing"
+        self.finished=False
+        self.cellChanged=False
         self.playingButtons=[]
         self.menuButtons=[]
         self.endButtons=[]
         self.lockedCells=[]
+        self.incorrectCells=[]
         self.font=pygame.font.SysFont("arial",cellSize//2)
         self.load()
     
@@ -32,6 +35,8 @@ class App:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 self.running=False
+
+            #user clicks
             if event.type==pygame.MOUSEBUTTONDOWN:
                 selected=self.mouseOnGrid()
                 if selected:
@@ -39,12 +44,27 @@ class App:
                 else:
                     print("not on board")
                     self.selected=None
-
+            #user types a key
+            if event.type==pygame.KEYDOWN:
+                if self.selected!= None and self.selected not in self.lockedCells:
+                    if self.isInt(event.unicode):
+                        #cell changed
+                        self.grid[self.selected[1]][self.selected[0]]=int(event.unicode)
+                        self.cellChanged=True
     
     def playing_update(self):
         self.mousePos=pygame.mouse.get_pos()
         for button in self.playingButtons:
             button.update(self.mousePos)
+
+        if self.cellChanged:
+            self.incorrectCells=[]
+            if self.allCellsDone():
+                #check if board is correct
+                self.checkAllCells()
+                print(self.incorrectCells)
+
+        
 
     def playing_draw(self):
         self.window.fill(WHITE)
@@ -56,18 +76,50 @@ class App:
             self.drawSelection(self.window,self.selected)
 
         self.shadeLockedCells(self.window,self.lockedCells)
-        
+        self.shadeIncorrectCells(self.window,self.incorrectCells)
 
         self.drawNumbers(self.window)
 
         self.drawGrid(self.window)
         pygame.display.update()
-### HELPER FUNCTION
+        self.cellChanged=False
+###BOARD checking Functions####
+    def allCellsDone(self):
+        for row in self.grid:
+            for number in row:
+                if number==0:
+                    return False
+        return True
+    
+    def checkAllCells(self):
+        self.checkRows()
+        #self.checkCols()
+        #self.checkSmallGrid()
+    
+    def checkRows(self):
+        for yidx, row in enumerate(self.grid):
+            possibles=[1,2,3,4,5,6,7,8,9]
+            for xidx in range(9):
+                if self.grid[yidx][xidx] in possibles:
+                    possibles.remove(self.grid[yidx][xidx])
+                else:
+                    if [xidx,yidx] not in self.lockedCells:
+                        self.incorrectCells.append([xidx,yidx])
+
+
+
+
+
+
+
+##### HELPER FUNCTION#####
     def shadeLockedCells(self,window,locked):
         for cell in locked:
             pygame.draw.rect(window,LOCKEDCELLCOLOUR,(cell[0]*cellSize+gridPos[0],cell[1]*cellSize+gridPos[1],cellSize,cellSize))
 
-
+    def shadeIncorrectCells(self,window,incorrect):
+        for cell in incorrect:
+            pygame.draw.rect(window,INCORRECTCELLCOLOUR,(cell[0]*cellSize+gridPos[0],cell[1]*cellSize+gridPos[1],cellSize,cellSize))
 
 
     def drawNumbers(self,window):
@@ -119,5 +171,12 @@ class App:
             for xidx, num in enumerate(row):
                 if num!=0:
                     self.lockedCells.append([xidx,yidx])
+
+    def isInt(self,string):
+        try:
+            int(string)
+            return True
+        except:
+            return False
         
 
